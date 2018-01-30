d0d43b7a9a31e4f4b7244b92afc4e85c93145938
Bhushan <bhushan5489@gmail.com>
2018-01-30 23:16:57 +0530

    Removed Storyboard from project and set up root view

---
+++ iOS-Code-Exercise/Views/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  iOS-Code-Exercise
+//
+//  Created by Bhushan Shinde on 30/01/18.
+//  Copyright © 2018 Bhushan Shinde. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
